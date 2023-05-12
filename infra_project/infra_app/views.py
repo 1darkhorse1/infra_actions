409e66d62cd655eb863b4a760e0c624be3575242
Ilya <nikulin-ilya-1993@mail.ru>
2023-05-12 06:50:39 +0300

    New message.

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось! Давай пока без бота..')
 
 
 def second_page(request):
